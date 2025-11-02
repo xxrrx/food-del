2513ed06d67d1e9a117db759504ccb880b38ebbd
xxrrx <hellothuan12112004@gmail.com>
2025-11-02 18:45:39 +0700

    Refactor code structure for improved readability and maintainability

--- fontend/src/components/Header/Header.jsx
+++ fontend/src/components/Header/Header.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Header.css'
+
 const Header = () => {
+  const viewerRef = useRef(null)
+  
+  useEffect(() => {
+  }, [])
+
   return (
     <div className='header'>
-        <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cupiditate odit animi beatae consectetur neque possimus cumque dolore minus iusto sequi autem dignissimos deleniti quae fugiat pariatur ad, eos incidunt?</p>
-            <button>View Menu</button>
+      <div className="header-contents">
+        <div className="text-content">
+          <h2>Order your favourite food here</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore cupiditate odit animi beatae consectetur neque possimus cumque dolore minus iusto sequi autem dignissimos deleniti quae fugiat pariatur ad, eos incidunt?</p>
+          <button>View Menu</button>
         </div>
+        <div className="spline-container">
+          <spline-viewer 
+            ref={viewerRef}
+            url="https://prod.spline.design/EaMqzE709AqwZCMP/scene.splinecode"
+          ></spline-viewer>
+        </div>
+      </div>
     </div>
   )
 }
